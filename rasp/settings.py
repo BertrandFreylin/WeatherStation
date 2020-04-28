2538bdfef384617eb247f0b512c2cf2b99d464ba
Bertrand <bfreylin@telaria.com>
2020-04-27 20:29:12 -0400

    Add allowed host

--- rasp/settings.py
+++ rasp/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-from rasp.local_settings import SECRET_KEY
+from rasp.local_settings import SECRET_KEY, ALLOWED_HOSTS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +27,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS
 
 
 # Application definition
